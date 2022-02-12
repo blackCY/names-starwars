de981f15bb21bcd56e8269fd6fa3bc7c0df2d2ee
blackCY <dllnacy@163.com>
2022-02-12 17:27:08 +0800

    将测试覆盖发送给codecov

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const starWarsNames = require('./starwars-names.json');
 
 const random = () => starWarsNames[(Math.random() * starWarsNames.length) | 0];
 
+// 这个函数没有被用到，他将影响我们的测试覆盖率
+// function doSomething() {
+//   console.log('hi');
+// }
+
 module.exports = {
   all: starWarsNames,
   random,
